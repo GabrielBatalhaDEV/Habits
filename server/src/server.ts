b9aca2deff181f1e03e79ca30ce6b45477a61bb9
Gabriel Batalha <gabrielbatalha0902@gmail.com>
2023-01-20 18:38:15 -0300

    Updated Api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,11 @@
 import fastify from "fastify";
-import cors from "@fastify/cors"
+import cors from "@fastify/cors";
+import { appRoutes } from "./routes";
 
 const app = fastify();
 
-app.register(cors)
-
-app.get("/", () => {
-  return "Hello World";
-});
+app.register(cors);
+app.register(appRoutes);
 
 app
   .listen({
