6c568fbe9efefcb820b7a05981a1f9e137ff5d92
Gabriel Batalha <gabrielbatalha0902@gmail.com>
2023-01-22 17:31:03 -0300

    Updated prisma

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../src/lib/prisma";
+/*import { prisma } from "../src/lib/prisma";
 
 const firstHabitId = "0730ffac-d039-4194-9571-01aa2aa0efbd";
 const firstHabitCreationDate = new Date("2022-12-31T03:00:00.000");
@@ -9,12 +9,13 @@ const secondHabitCreationDate = new Date("2023-01-03T03:00:00.000");
 const thirdHabitId = "fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00";
 const thirdHabitCreationDate = new Date("2023-01-08T03:00:00.000");
 
+*/
+
+/*
 async function run() {
   await prisma.habit.deleteMany();
   await prisma.day.deleteMany();
-  /**
-   * Create habits
-   */
+
   await Promise.all([
     prisma.habit.create({
       data: {
@@ -55,14 +56,12 @@ async function run() {
       },
     }),
   ]);
-
+  */
+/*
   await Promise.all([
-    /**
-     * Habits (Complete/Available): 1/1
-     */
+    
     prisma.day.create({
       data: {
-        /** Monday */
         date: new Date("2023-01-02T03:00:00.000z"),
         dayHabits: {
           create: {
@@ -72,12 +71,9 @@ async function run() {
       },
     }),
 
-    /**
-     * Habits (Complete/Available): 1/1
-     */
     prisma.day.create({
       data: {
-        /** Friday */
+
         date: new Date("2023-01-06T03:00:00.000z"),
         dayHabits: {
           create: {
@@ -87,12 +83,10 @@ async function run() {
       },
     }),
 
-    /**
-     * Habits (Complete/Available): 2/2
-     */
+
     prisma.day.create({
       data: {
-        /** Wednesday */
+
         date: new Date("2023-01-04T03:00:00.000z"),
         dayHabits: {
           create: [{ habit_id: firstHabitId }, { habit_id: secondHabitId }],
@@ -111,3 +105,4 @@ run()
     await prisma.$disconnect();
     process.exit(1);
   });
+*/
